b2547b04576410dd35ea88a764fdb4a612b987ff
kiranG <kiran.ambadas.gaikwad@gmail.com>
2025-07-30 17:05:22 +0530

    initial commit

---
+++ src/main/java/com/spotbugs/demo/SpotbugsApplication.java
@@ -0,0 +1,13 @@
+package com.spotbugs.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotbugsApplication {
+
+	public static void Main(String[] args) {
+		SpringApplication.run(SpotbugsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spotbugs/demo/SpotbugsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotbugs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotbugsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
